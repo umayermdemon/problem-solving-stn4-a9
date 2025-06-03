f87c12e8c5fd6bb3263370f1e4b3f86c5aaa6270
umayermdemon <mamudmdemon@gmail.com>
2025-06-03 22:21:49 +0600

    feat: task-6 complete

--- problemSolving.js
+++ problemSolving.js
@@ -109,6 +109,14 @@ console.log(foundPerson);
 // 6.Task: Array Reduction
 // -Create an array of numbers. Write a function that uses the reduce method to calculate the sum of all even numbers in the array.
 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const sumByReduceMethod = (array) => {
+  return array.reduce((sum, num) => (num % 2 === 0 ? sum + num : sum), 0);
+};
+
+const sumOfArray = sumByReduceMethod(arr);
+console.log(sumOfArray);
+
 // 7. Task: Leap Year Checker
 // -Write a function that determines whether a given year is a leap year.
 // Example: Happy New Year
