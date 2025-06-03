64b5e11c284d129e142a53991a2622cd4227a7b7
umayermdemon <mamudmdemon@gmail.com>
2025-06-03 16:42:30 +0600

    feat: task-1 complete

---
+++ problemSolving.js
@@ -0,0 +1,63 @@
+// 1.Task: Array Filtering and Mapping
+// -Create an array of objects, each representing a person with properties like name, age, and gender. Write a function to filter out all females and then map the remaining people to an array of names. Print the final result.
+
+const persons = [
+  {
+    name: "Jannat",
+    age: 5,
+    gender: "female",
+  },
+  {
+    name: "Hossain",
+    age: 3,
+    gender: "male",
+  },
+  {
+    name: "Usama",
+    age: 3,
+    gender: "male",
+  },
+  {
+    name: "Jayan",
+    age: 1,
+    gender: "male",
+  },
+  {
+    name: "Sumaiya",
+    age: 0.5,
+    gender: "female",
+  },
+];
+
+const malePersonsName = persons
+  .filter((data) => data.gender != "female")
+  .map((malePerson) => malePerson.name);
+console.log(malePersonsName);
+
+// 2.Task: Object Manipulation
+// -Create an array of objects representing books with properties like title, author, and year. Write a function that takes the array and returns a new array with only the book titles. Print the result.
+
+// 3.Task: Function Composition
+// -Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5.
+
+// 4.Task: Sorting Objects
+// -Create an array of objects representing cars with properties like make, model, and year. Write a function to sort the array of cars by the year of manufacture in ascending order. Print the sorted array.
+
+// 5.Task: Find and Modify
+// -Write a function that searches an array of objects for a specific person by name. If found, modify their age property. Print the updated array.
+
+// 6.Task: Array Reduction
+// -Create an array of numbers. Write a function that uses the reduce method to calculate the sum of all even numbers in the array.
+
+// 7. Task: Leap Year Checker
+// -Write a function that determines whether a given year is a leap year.
+// Example: Happy New Year
+
+// 8. Task: Unique Values
+// -Create an array of numbers with some duplicate values. Write a function to filter out the duplicate values and return a new array with only unique numbers. Print the result.
+
+// 09.Task: Advanced Sorting
+// -Create an array of objects representing students with 'name' and 'grades' properties. Write a function to sort the students by average grade in descending order.
+
+// 10. Task: Functional Programming - Reduce
+// -Write a function that uses the reduce function to calculate the total value of an array of objects with a 'quantity' and 'price' property.interview
